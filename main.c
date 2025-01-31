2c5c9c3b9ebf7a7e0b8d8c39c8265dd8d3af16b3
ncyerage23 <ncyerage@gmail.com>
2025-01-31 14:37:59 -0500

    got a window

---
+++ main.c
@@ -0,0 +1,124 @@
+//Starting file for Proto Engine
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+#define SCREEN_WIDTH    850
+#define SCREEN_HEIGHT   650
+#define FOV_SCALE       SCREEN_WIDTH / 2
+
+#define MOVE_SPEED      5.0
+#define ROT_SPEED       0.01
+
+
+typedef struct {
+    int r;
+    int g;
+    int b;
+    int a;
+} color_t; 
+
+typedef struct {
+    float x, y;
+} vector_t;
+
+typedef struct {
+    vector_t p1, p2;
+    int portal;
+    int sector;
+} wall_t;
+
+typedef struct {
+    int id;
+    int first_wall, num_walls;
+    float zfloor, zceil;
+} sector_t;
+
+
+
+static struct {
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    int quit;
+
+    // static struct {
+    //     vector_t pos;
+    //     float zpos;
+    //     float angle;
+    //     int sector;
+    // } camera;
+
+} control;
+
+
+
+int init() {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    control.window = SDL_CreateWindow(
+        "No idea!", 
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+        SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN
+    );
+
+    if (!control.window) {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    control.renderer = SDL_CreateRenderer(control.window, -1, SDL_RENDERER_ACCELERATED);
+    if (!control.renderer) {
+        printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(control.window);
+        SDL_Quit();
+        return 1;
+    }
+
+    control.quit = 0;
+    return 0;
+}
+
+
+
+int main() {
+    if (init()) {
+        printf("Init failed.");
+        return 1;
+    }
+    
+    SDL_Event e;
+    while (!control.quit) {
+
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                control.quit = 1;
+            }
+        }
+
+        // Paints the screen black
+        SDL_SetRenderDrawColor(control.renderer, 0, 0, 0, 255);
+        SDL_RenderClear(control.renderer);
+
+        SDL_SetRenderDrawColor(control.renderer, 255, 0, 0, 255);
+        SDL_Rect rect = { 200, 150, 400, 300 };
+        SDL_RenderFillRect(control.renderer, &rect);
+
+
+        // Present the renderer (show the content on the window)
+        SDL_RenderPresent(control.renderer);
+    }
+
+    SDL_DestroyRenderer(control.renderer);
+    SDL_DestroyWindow(control.window);
+    SDL_Quit();
+    return 0;
+}
+
+
+
+
